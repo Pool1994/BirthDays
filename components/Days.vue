8c25214059a06c3faa2eea917974a4bbef5003ae
Enrique Saenz <enrique.saenz@alarcongroup.us>
2023-09-19 20:06:04 -0500

    add compoenents

---
+++ components/Days.vue
@@ -0,0 +1,24 @@
+<script>
+const props = defineProps('list')
+</script>
+<template>
+    <div class="overflow-x-auto">
+  <table class="table">
+    <!-- head -->
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Name</th>
+        <th>Birthday</th> 
+      </tr>
+    </thead>
+    <tbody> 
+      <tr v-for="item in props.data" :key="item.id">
+        <td> {{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.birthday}}</td>
+      </tr> 
+    </tbody>
+  </table>
+</div>
+</template>
---
+++ components/Month.vue
@@ -0,0 +1,61 @@
+<script>
+const data = ref([
+    {id:1,
+    name:"Cy Ganderton",
+    birthday:"Quality Control",
+    },
+    {id:2,
+    name:"Cy Ganderton",
+    birthday:"Quality Control",
+    },
+    {id:3,
+    name:"Cy Ganderton",
+    birthday:"Quality Control",
+    },
+    {id:4,
+    name:"Cy Ganderton",
+    birthday:"Quality Control",
+    },
+    {id:5,
+    name:"Cy Ganderton",
+    birthday:"Quality Control",
+    },
+    {id:6,
+    name:"Cy Ganderton",
+    birthday:"Quality Control",
+    }
+])
+</script>
+
+
+<template>
+    <div class="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" checked="checked" /> 
+  <div class="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div class="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+<div class="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" /> 
+  <div class="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div class="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+<div class="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" /> 
+  <div class="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div class="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+
+<Month :data="data" />
+</template>
