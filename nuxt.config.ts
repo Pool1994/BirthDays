51e112262279245140e5b791d56c871c4e190671
Pool <pool.vera@alarcongroup.us>
2023-09-21 15:31:28 -0500

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules:['@nuxtjs/tailwindcss'],
+  app:{
+    baseURL:"/BirthDays/"
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -33,6 +33,11 @@ const save = async () => {
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="">
+        <div>
+          <button class="btn btn-primary" @click="navigateTo('/birth')">
+            Ir a Lista
+          </button>
+        </div>
         <h1 class="text-5xl font-bold">Hello there</h1>
         <p class="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
